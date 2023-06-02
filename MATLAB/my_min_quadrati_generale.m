de1121785382ddb0902a24fc974f69b8a75f79f1
salor02 <salvadil02@gmail.com>
2023-06-02 10:09:39 +0200

    .

---
+++ MATLAB/my_min_quadrati_generale.m
@@ -0,0 +1,32 @@
+% minimi quadrati
+% verificare m>=n e rango(a) = n
+% definire a come vandermorth
+% fattorizzazione qr matrice A
+% calcolo y segnato = Qty 
+% estrarre solo prime n componenti di y
+% risolvere sistema triangolare sup R alfa = y segnato
+
+function alfa = my_min_quadrati_generale(x,y,f)
+    m = length(x);
+
+    if(m<n)
+        error("numero di dati minore del grado del polinomio");
+    end
+
+    A = ones(m,n);
+    for i = 2:n
+        A(:,i) = f{i}(x);
+    end
+
+    if(rank(A) ~= n)
+        error("rango di A diverso da n");
+    end
+
+    [Q,R] = qr(A);
+    R = R(1:n,1:n);
+    
+    yseg = Q' * y;
+    yseg = yseg(1:n);
+
+    alfa = my_utri_sol(R,yseg);
+end
--- MATLAB/my_vandermonde.m
+++ MATLAB/my_vandermonde.m
@@ -1,12 +1,20 @@
-% Funzione che prende in ingresso un vettore x di lunghezza n
-% che rappresenta le ascisse dei punti di interpolazione (nodi)
-% e restituisce la matrice nxn di Vandermonde.
+% Si scriva una funzione my_vandermonde.m che, dati n+1 punti del piano R^2, restituisca la matrice di Vandermonde V di dimensione (n+1)x(n+1).
+% 
+% La funzione prende in ingresso:
+% - un vettore colonna di nodi (ascisse dei punti di interpolazione) x,
+% 
+% Si costruisca la matrice V per colonna:
+% - inizializzare V a una matrice di 1 (utilizzando il comando ones)
+% - tranne la prima, ogni colonna si ottiene moltiplicando (elemento per elemento, ovvero puntualmente .*) la colonna precedente per il vettore dei nodi.
+% V(:,j) = V(:,j-1) .* x, per ogni j=2,..,n+1.
+% Si noti che n+1 è la lunghezza del vettore in input.
 
-function V = my_vandermonde(x)
+function V = my_vandermonde(x_nodi)
+    N = length(x_nodi);
 
-n = length(x);
-V = ones( n );
+    V = ones(N);
 
-for j = 2 : n
-    V(:,j) = V(:,j-1) .* x;
+    for j = 2 : N
+        V(:, j) = V(:, j-1) .* x_nodi;
+    end
 end
