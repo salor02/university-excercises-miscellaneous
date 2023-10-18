f6b9835336aa5735fb8ad2d1b8953168bd0cc735
Salvatore Di Lorenzo <salvadil02@gmail.com>
2023-10-18 23:48:55 +0200

    finito homework01 prima parte ma mancano commenti

--- linguaggi-compilatori/homework01/parte1/main.cpp
+++ linguaggi-compilatori/homework01/parte1/main.cpp
@@ -1,11 +1,5 @@
 /*
-0. trovare stato iniziale altrimenti non so da dove iniziare
-1. esaminare input in ordine
-2. costruire set degli stati del D seguendo ricorsivamente epsilon
-3. una volta costurito il primo set fare punto 2 per ogni simbolo dell'alfabeto
-4. salvare ogni nuovo stato in un set e ogni transazione in un map<set,char> = set
-
-TODO: leggere alfabeto anche non char e capire perche mi legge una riga in meno
+TODO: leggere alfabeto anche non char, prototipi e commenti
 
 NB:
 1.  Il programma funziona solo se gli stati dell'automa non deterministico in input sono numeri interi 
@@ -14,17 +8,14 @@ NB:
 */
 
 #include <iostream>
-#include <bits/stdc++.h>
 #include <utility>
+#include <map>
 #include <set>
 #include <vector>
 #include <queue>
 #include <fstream>
 #include <sstream>
 #include <algorithm>
-
-#include <time.h>
-#include <unistd.h>
 
 using namespace std;
 
@@ -34,14 +25,14 @@ struct AFND{
     map<pair<int, char>, set<int>> data;
     string alpha;
     int state_number;
-    int final_state_number; //non utilizzato
+    set<int> final_state;
 };
 
 struct AFD{
-    map<pair<set<int>, char>, int> data;
+    map<pair<int, char>, int> data;
     string alpha;
     vector<set<int>> state_list;
-    int final_state_number; //non utilizzato
+    set<int> final_state;
 };
 
 void upload_AFND(AFND &automaND, ifstream &f){
@@ -53,13 +44,20 @@ void upload_AFND(AFND &automaND, ifstream &f){
     line.erase(remove_if(line.begin(), line.end(), ::isspace), line.end());
     automaND.alpha = EPS + line;
     
-    //lettura del numero degli stati finali, inutile al momento
+    //lettura degli stati finali
     getline(f, line);
+
+    istringstream iss(line);
+    int final_state;
+
+    while(iss>>final_state){
+        automaND.final_state.insert(final_state);
+    }
 
     //costruzione della struttura dati
     int stato;
     char input;
-    string dest_state;
+    int dest_state;
 
     for(int i = 0; getline(f, line); i++){
         //definizione stato e input corrente in base al numero di riga e alla cardinalità dell'alfabeto
@@ -73,8 +71,8 @@ void upload_AFND(AFND &automaND, ifstream &f){
         pair<int, char> transition(stato, input);
 
         set<int> dest;
-        while(getline(iss, dest_state, ' ')){
-            dest.insert(stoi(dest_state));
+        while(iss>>dest_state){
+            dest.insert(dest_state);
         }
 
         //assegnazione risultato lettura linea corrente alla struttura dati
@@ -84,8 +82,8 @@ void upload_AFND(AFND &automaND, ifstream &f){
     //assegnazione numero di stati trovati
     automaND.state_number = stato;
 
-    //stampa
-    for(auto item:automaND.data){
+    //stampa debug
+    /*for(auto item:automaND.data){
         cout<<item.first.first;
         cout<<'\t';
         cout<<item.first.second;
@@ -95,10 +93,10 @@ void upload_AFND(AFND &automaND, ifstream &f){
             cout<<' ';
         }
         cout<<endl;
-    }
+    }*/
 }
 
-bool get_initial_state(AFND &automaND, int &initial_state){
+bool get_initial_state(const AFND &automaND, int &initial_state){
     
     //creazione di un set degli stati
     set<int> state;
@@ -116,6 +114,7 @@ bool get_initial_state(AFND &automaND, int &initial_state){
     //se tutto è andato bene il set dovrebbe avere un solo elemento corrisponendente proprio allo stato inziale
     if(state.size() == 1){
         for(auto item:state){
+            initial_state = item;
             return true;
         }
     }
@@ -123,11 +122,11 @@ bool get_initial_state(AFND &automaND, int &initial_state){
     return false;
 }
 
-void explore_eps(AFND &automaND, int initial_state, set<int> &state){
+void explore_eps(const AFND &automaND, int initial_state, set<int> &state){
     
     state.insert(initial_state);
     
-    set<int> epsilon_set = automaND.data[make_pair(initial_state, EPS)];
+    set<int> epsilon_set = automaND.data.at(make_pair(initial_state, EPS));
 
     if(epsilon_set.size() < 1) return; //nessuna destinazione raggiungibile tramite EPS per questo punto iniziale
 
@@ -136,54 +135,110 @@ void explore_eps(AFND &automaND, int initial_state, set<int> &state){
     }
 }
 
-bool conversion(AFND &automaND, AFD &automaD, int initial_state){
+bool conversion(const AFND &automaND, AFD &automaD, const int initial_state){
 
+    //i due alfabeti dei simboli sono uguali eccetto che per epsilon
     automaD.alpha = automaND.alpha;
+    automaD.alpha.erase(0,1);
     
-    //crea set con tutti gli stati raggiungibili senza input (epsilon)
+    //coda degli stati da analizzare
     queue<set<int>> stateD_queue;
 
-    set<int> current_state;
-    set<int> discovered_state;
-    explore_eps(automaND, initial_state, current_state);
-    stateD_queue.push(current_state);
+    //set di appoggio per gestire gli stati
+    set<int> current_state; //stato analizzato correntemente dal ciclo di seguito
+    set<int> discovered_state; //stato scoperto percorrendo l'automa ND
 
+    //costruzione dello stato iniziale dell'automa D
+    explore_eps(automaND, initial_state, discovered_state);
+    stateD_queue.push(discovered_state);
+    automaD.state_list.push_back(discovered_state);
+
+    //ciclo fino a che la coda degli stati da analizzare non si svuota
     while(!stateD_queue.empty()){
         current_state = stateD_queue.front();
         
+        //vengono analizzate tutte le transizioni di tutti gli elementi di current_state (tranne le epsilon) 
+        for(int i = 0; i < automaD.alpha.size(); i++){
+            discovered_state.clear();
 
-        for(int i = 1; i < automaD.alpha.size(); i++){
             char current_input = automaD.alpha[i];
             for(auto item:current_state){
-                set<int> current_output = automaND.data[make_pair(item, current_input)];
+                set<int> current_output = automaND.data.at(make_pair(item, current_input));
                 for(auto output:current_output){
+                    /*viene eseguita questa procedura su tutti gli stati dell'automa ND in modo tale da creare un nuovo stato 
+                    per l'automaD che deve comprendere, quindi, anche le epsilon transizioni (analizzate ricorsivamente in questo caso)*/
                     explore_eps(automaND, output, discovered_state);
                 }
             }
-            for(auto item:discovered_state) cout<<item<<" ";
-            cout<<endl;
-            if(find(automaD.state_list.begin(), automaD.state_list.end(), discovered_state) == automaD.state_list.end() && discovered_state.size() > 0){
-                stateD_queue.push(discovered_state);
-                automaD.state_list.push_back(discovered_state);
-            }
 
-            discovered_state.clear();
-                
+            /*lo stato scoperto viene aggiunto alla coda solo se non è gia presente nella lista degli stati dell'automa D (marked) oppure se è uno stato vuoto
+            questo implica che la transizione analizzata non esiste*/
+            auto discovered_itr = find(automaD.state_list.begin(), automaD.state_list.end(), discovered_state);
+            int discovered_output;//identifica lo stato di arrivo della transizione
+
+            if(discovered_state.size() > 0){
+                if(discovered_itr == automaD.state_list.end()){
+                    stateD_queue.push(discovered_state);
+                    automaD.state_list.push_back(discovered_state);
+                    discovered_output = automaD.state_list.size() - 1;
+                }
+                else{
+                    discovered_output = distance(automaD.state_list.begin(), discovered_itr);
+                }
+
+                /*viene registrata una transizione dallo stato corrente allo stato discovered. Se quest'ultimo è già presente nella lista degli stati
+                verrà cercato e ottenuto il suo indice altrimenti viene preso come indice la size della lista dato che è sicuramente l'ultimo stato inserito*/
+                auto current_itr = find(automaD.state_list.begin(), automaD.state_list.end(), current_state);
+                automaD.data[make_pair(distance(automaD.state_list.begin(), current_itr), current_input)] = discovered_output;  
+            }
         }
-        cout<<stateD_queue.size()<<endl;
-        //sleep(1);
+
+        //dequeue dello stato esaminato
         stateD_queue.pop();
-        
     }
 
-    cout<<"Discovered states number: "<<automaD.state_list.size()<<endl;
+    //definizione stati finali dell'automa D
+    for(int i = 0; i < automaD.state_list.size(); i++){
+        for(auto state:automaD.state_list[i]){
+            /*se almeno uno degli elementi dello stato D appartiene al set degli stati finali di ND allora
+            lo stato D diventerà uno stato finale di D*/
+            if(automaND.final_state.find(state) != automaND.final_state.end()){
+                automaD.final_state.insert(i);
+            }
+        }
+    }
+
+    return true;
+}
+
+void printAFD(const AFD &automaD, ostream &os){
+    //stampa degli stati
     for(auto state:automaD.state_list){
         for(auto state_item:state){
-            cout<<state_item<<" ";
+            os<<state_item<<" ";
         }
-        cout<<endl;
+        os<<endl;
     }
-    return true;
+
+    //stampa degli stati finali
+    for(auto state:automaD.final_state){
+        os<<state<<" ";
+    }
+    os<<endl;
+
+    //stampa delle transizioni
+    for(int state = 0; state < automaD.state_list.size(); state++){
+        for(auto input:automaD.alpha){
+
+            //blocco try catch utilizzato perche at() solleva un'eccezione se la chiave fornita non è a associata a nessun valore
+            try{
+                os<<automaD.data.at(make_pair(state,input))<<endl;
+            }
+            catch(out_of_range exception){
+                os<<endl;
+            }
+        }
+    }
 }
 
 int main(int argc, char *argv[]){
@@ -193,7 +248,7 @@ int main(int argc, char *argv[]){
 
     if(argc > 1){
 
-        //FILE OPENING
+        //INPUT FILE OPENING
         string file_name = argv[1];
         cout<<"Trying to open file -- " + file_name<<endl;
         ifstream f;
@@ -225,6 +280,24 @@ int main(int argc, char *argv[]){
         }
 
         cout<<"Conversion completed!\n";
+
+        //STAMPA SU FILE
+        if(argc > 2){
+            string file_name = argv[2];
+            cout<<"Trying to open file -- " + file_name<<endl;
+            ofstream f;
+            f.open(file_name);
+
+            cout<<"Saving on file '"<<file_name<<"' ...\n";
+            printAFD(automaD, f);
+        }
+        else{
+            cout<<"No output file provided, will write result on default output stream\n";
+            printAFD(automaD, cout);
+        }
+        
+
+        cout<<"Save complete!\n";
         return 0;
     }
 
