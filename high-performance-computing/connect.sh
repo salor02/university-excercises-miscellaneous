2b2380d6593668f47f7bef4848cf5c64e42dcd25
salor02 <salvadil02@gmail.com>
2024-10-31 12:29:00 +0100

    script connect hpc

--- high-performance-computing/connect.sh
+++ high-performance-computing/connect.sh
@@ -5,5 +5,14 @@ local_path='./board4'
 port=22704
 user=301414
 
-sshfs -p $port "$user"@elgamal.mat.unimo.it:"$remote_path" "$local_path"
-ssh -p $port "$user"@elgamal.mat.unimo.it
+if [ "$1" = "tunnel" ]; then
+    remote_host='localhost'
+    autossh -f -N -L 22704:"$remote_host":22704 student@wks-marongiu2.mat.unimo.it -v
+else
+    remote_host='elgamal.mat.unimo.it'
+fi
+
+sshfs -p $port "$user"@"$remote_host":"$remote_path" "$local_path"
+ssh -p $port "$user"@"$remote_host"
+
+    
